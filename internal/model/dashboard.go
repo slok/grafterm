0d7524649ea1add55e46190db9501c76f6850910
Xabier Larrakoetxea <slok69@gmail.com>
2019-04-06 14:09:26 +0200

    Add model dashbaord
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

---
+++ internal/model/dashboard.go
@@ -0,0 +1,53 @@
+package model
+
+// Dashboard represents a dashboard.
+type Dashboard struct {
+	Rows []Row `json:"rows,omitempty"`
+}
+
+// Row represents a row.
+type Row struct {
+	Title   string   `json:"title,omitempty"`
+	Border  bool     `json:"border,omitempty"`
+	Widgets []Widget `json:"widgets,omitempty"`
+}
+
+// Widget represents a widget.
+type Widget struct {
+	WidgetSource `json:",inline"`
+}
+
+// WidgetSource will tell what kind of widget is.
+type WidgetSource struct {
+	SingleStat *SingleStatWidgetSource `json:"singleStat,omitempty"`
+	Gauge      *GaugeWidgetSource      `json:"gauge,omitempty"`
+}
+
+// SingleStatWidgetSource represents a simple value widget.
+type SingleStatWidgetSource struct {
+	Query      Query       `json:"query,omitempty"`
+	Thresholds []Threshold `json:"thresholds,omitempty"`
+}
+
+// GaugeWidgetSource represents a simple value widget in donut format.
+type GaugeWidgetSource struct {
+	Query        Query       `json:"query,omitempty"`
+	PercentValue bool        `json:"percentValue,omitempty"`
+	Max          int         `json:"max,omitempty"`
+	Min          int         `json:"min,omitempty"`
+	Thresholds   []Threshold `json:"thresholds,omitempty"`
+}
+
+// Query is the query that will be made to the datasource.
+type Query struct {
+	Query string `json:"query,omitempty"`
+	// TODO(slok): Datasources.
+}
+
+// Threshold is a color threshold that is composed
+// with the start value, 0 means the base or starting
+// threshold.
+type Threshold struct {
+	StartValue float64 `json:"startValue"`
+	Color      string  `json:"color"`
+}
