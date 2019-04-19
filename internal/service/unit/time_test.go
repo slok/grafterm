a7911735b3bfb5265be15ce790eb7887832f1b02
Xabier Larrakoetxea <slok69@gmail.com>
2019-04-19 11:25:17 +0200

    Add time duration service funcs
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

---
+++ internal/service/unit/time_test.go
@@ -0,0 +1,115 @@
+package unit_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/slok/meterm/internal/service/unit"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNearestDurationFromSteps(t *testing.T) {
+	tests := []struct {
+		name      string
+		timeRange time.Duration
+		steps     int
+		expDur    time.Duration
+	}{
+		{
+			name:      "A low range with lots of steps should return the min interval",
+			timeRange: 2 * time.Hour,
+			steps:     1000,
+			expDur:    30 * time.Second,
+		},
+		{
+			name:      "A high range with few steps should return the max interval",
+			timeRange: 5000 * time.Hour,
+			steps:     2,
+			expDur:    720 * time.Hour,
+		},
+		{
+			name:      "calculates the nearest (12h, 50 steps).",
+			timeRange: 12 * time.Hour,
+			steps:     50,
+			expDur:    10 * time.Minute,
+		},
+		{
+			name:      "calculates the nearest (2h, 50 steps).",
+			timeRange: 2 * time.Hour,
+			steps:     50,
+			expDur:    2 * time.Minute,
+		},
+		{
+			name:      "calculates the nearest (30m, 50 steps).",
+			timeRange: 30 * time.Minute,
+			steps:     50,
+			expDur:    30 * time.Second,
+		},
+		{
+			name:      "calculates the nearest (6h, 30 steps).",
+			timeRange: 6 * time.Hour,
+			steps:     30,
+			expDur:    10 * time.Minute,
+		},
+		{
+			name:      "calculates the nearest (3d, 50 steps).",
+			timeRange: 72 * time.Hour,
+			steps:     50,
+			expDur:    1 * time.Hour,
+		},
+		{
+			name:      "calculates the nearest (7d, 50 steps).",
+			timeRange: 168 * time.Hour,
+			steps:     50,
+			expDur:    3 * time.Hour,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			gotDur := unit.NearestDurationFromSteps(test.timeRange, test.steps)
+			assert.Equal(t, test.expDur, gotDur)
+		})
+	}
+}
+
+func TestDurationToSimpleString(t *testing.T) {
+	tests := []struct {
+		name      string
+		timeRange time.Duration
+		exp       string
+	}{
+		{
+			name:      "Seconds.",
+			timeRange: 39 * time.Second,
+			exp:       "39s",
+		},
+		{
+			name:      "Minutes.",
+			timeRange: 39 * time.Minute,
+			exp:       "39m",
+		},
+		{
+			name:      "Minutes in seconds.",
+			timeRange: 61 * time.Second,
+			exp:       "61s",
+		},
+		{
+			name:      "Hours.",
+			timeRange: 39 * time.Hour,
+			exp:       "39h",
+		},
+		{
+			name:      "Hours in minutes.",
+			timeRange: 75 * time.Minute,
+			exp:       "75m",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := unit.DurationToSimpleString(test.timeRange)
+			assert.Equal(t, test.exp, got)
+		})
+	}
+}
