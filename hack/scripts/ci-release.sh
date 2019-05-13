ee9f8dacedaf4bf5c744b0b4fbd1068fabb85d07
Xabier Larrakoetxea <slok69@gmail.com>
2019-05-13 07:03:01 +0200

    Add publishing artifact
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

--- hack/scripts/ci-release.sh
+++ hack/scripts/ci-release.sh
@@ -2,13 +2,13 @@
 
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [[ -n ${TRAVIS_TAG} ]]; then
-    echo "Tag ${TRAVIS_TAG}. building releases..."
+if [[ -n ${CIRCLE_TAG} ]]; then
+    echo "Tag ${CIRCLE_TAG}. building releases..."
 
     archs=( Linux Darwin Windows )
     for arch in "${archs[@]}"
     do
-        VERSION=${TRAVIS_TAG} ostype=${arch} ${current_dir}/build.sh
+        VERSION=${CIRCLE_TAG} ostype=${arch} ${current_dir}/build.sh
     done
 else
     echo "no tag, skipping release..."
